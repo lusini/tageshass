38c247ac8fb8afd0c0d7397ba81f9c2382ca6ee2
Christoph Neuroth <christoph.neuroth@gmail.com>
2013-01-28 14:32:50 +0100

    leave exiting the process to the app

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var SMTPServer = require('./lib/smtp')
   , smtpServer = new SMTPServer();
 smtpServer.start(function (err) {
   if (err) {
-    console.log('Failed to start SMTP Server.');
-    process.exxit(1);
+    console.log('Failed to start SMTP Server:' + err);
+    process.exit(1);
   }
   console.log('Started SMTP server on ' + smtpServer.getPort());
 });
